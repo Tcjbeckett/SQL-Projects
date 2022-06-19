ea105647305ffe4c6344cbd3bfd77ac81909e098
Tcjbeckett <tcjbeckett@gmail.com>
2022-06-19 14:02:29 -0400

    Added loop to display menu method

--- Menu Application/Menu_app/src/main/java/Menu2.java
+++ Menu Application/Menu_app/src/main/java/Menu2.java
@@ -3,15 +3,18 @@ import  java.util.Scanner;
 
 public class Menu2 {
 
-    public static void displayMenu(){
+    public static void displayMenu(int options[], String menuItems []){ //passing in the Menu data arrays
         //Display Menu
         System.out.println("--------");
         System.out.println("M E N U");
         System.out.println("--------");
-        System.out.println("1. Spring Water");
-        System.out.println("2. Cherry Pepsi");
-        System.out.println("3. Orange Juice");
-        System.out.println("4. Exit");
+
+        //Loop through the Menu data options and menuItems
+        //Note* the options/menu item arrays are the same size
+        for(int i = 0; i < options.length; i++){
+            System.out.printf("%d. %s\n", options[i], menuItems[i]);  //printf to format the string better - %d is the placeholder for an int and %s is the placeholder for string - \n = newline
+        }
+
         System.out.println("===================");
         System.out.print("Enter choice: "); //didn't use a println so the input can be on the same line
 
@@ -24,8 +27,12 @@ public class Menu2 {
         //User Choice
         int choice;
 
+        //Menu data - stored in an array so we are not hard coding the displayed menu
+        int options[] = {1,2,3,4};
+        String menuItems [] = {"Spring Water", "Cherry Pepsi", "Orange Juice", "Exit"};
+
         //Display Menu
-        displayMenu();
+        displayMenu(options, menuItems); // passing in the two arrays to the display menu to compliment the loop we added to the display menu method
 
         //Get choice from user
         choice = scanner.nextInt();
@@ -44,7 +51,7 @@ public class Menu2 {
 
             //Display Menu Again (This should also be a method...this code is not dry)
             //a try catch would be here for wrong inputs as well as a do while, if it was a method
-            displayMenu();
+            displayMenu(options, menuItems); // passing in the two arrays to the display menu to compliment the loop we added to the display menu method
 
 
             //Get User Choice Again
