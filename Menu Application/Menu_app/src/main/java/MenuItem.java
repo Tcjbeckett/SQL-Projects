049bc9344f7c56305ee34ccd2ee8265050773133
Tcjbeckett <tcjbeckett@gmail.com>
2022-06-19 21:00:11 -0400

    Added MenuItem class

---
+++ Menu Application/Menu_app/src/main/java/MenuItem.java
@@ -0,0 +1,21 @@
+public class MenuItem {
+    
+    //instance variables
+    private String name;
+    private double price;
+
+    //constructor
+    MenuItem(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    //getters
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+}
