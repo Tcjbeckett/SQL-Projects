d5395733422ae2b08ad0eca1e182724891582e03
Tcjbeckett <tcjbeckett@gmail.com>
2022-07-02 18:56:44 -0400

    first commit

---
+++ New SQL Project/sql-api/sql-api/src/test/java/dev/tcjbeckett/sqlapi/SqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tcjbeckett.sqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
