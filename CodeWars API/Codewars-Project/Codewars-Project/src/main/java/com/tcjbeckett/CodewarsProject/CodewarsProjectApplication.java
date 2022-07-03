082f2ad9e03db801d6fb2b9c9326a0fc58e7cc19
Tcjbeckett <tcjbeckett@gmail.com>
2022-07-03 12:19:35 -0400

    first commit

---
+++ CodeWars API/Codewars-Project/Codewars-Project/src/main/java/com/tcjbeckett/CodewarsProject/CodewarsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tcjbeckett.CodewarsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewarsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodewarsProjectApplication.class, args);
+	}
+
+}
---
+++ CodeWars API/Codewars-Project/Codewars-Project/src/test/java/com/tcjbeckett/CodewarsProject/CodewarsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcjbeckett.CodewarsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewarsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
